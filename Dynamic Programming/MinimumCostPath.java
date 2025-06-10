a15ffa0b8aa1f80b60b8d050f2c9258298655de4
schrodingerkatt <arya222aya@gmail.com>
2025-06-10 15:20:04 +0530

    fixed bugs

--- Dynamic Programming/MinimumCostPath.java
+++ Dynamic Programming/MinimumCostPath.java
@@ -34,14 +34,14 @@ public class MinimumCostPath{
                 }
 
                 else if(i == dp.length-1){
-                    dp[i][j]+=dp[i][j+1];
+                    dp[i][j] = dp[i][j+1]+arr[i][j];
                 }
                 else if(j == dp[0].length-1){
-                    dp[i][j]+=dp[i-1][j];
+                    dp[i][j] = dp[i+1][j]+arr[i][j];
                 }
 
                 else{
-                    dp[i][j]+=Math.min(dp[i][j+1],dp[i+1][j]);
+                    dp[i][j] = arr[i][j]+Math.min(dp[i][j+1],dp[i+1][j]);
                 }
             }
         }
